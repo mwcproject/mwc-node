54ac1f372fbb40be9a23570c7ae3d7a7aec945a1
bayk <k@k.com>
2019-12-22 16:07:09 -0800

    Revert "very verbose build for mwc-node" This reverts commit 2696b084

--- build_static.sh
+++ build_static.sh
@@ -5,4 +5,4 @@ export OPENSSL_LIB_DIR=/usr/local/opt/openssl@1.1/lib/;
 export OPENSSL_INCLUDE_DIR=/usr/local/opt/openssl@1.1/include;
 export OPENSSL_STATIC=yes
 
-cargo build --verbose --verbose --release
+cargo build --release
