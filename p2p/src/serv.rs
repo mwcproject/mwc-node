fdbc77eb364c0257b7708af9e94996dede5bfd66
kbay <kbay@vmware.com>
2020-01-27 20:50:39 -0800

    Fix false alarm errors logs messages

--- p2p/src/serv.rs
+++ p2p/src/serv.rs
@@ -61,7 +61,12 @@ impl Server {
 			config: config.clone(),
 			capabilities: capab,
 			handshake: Arc::new(Handshake::new(genesis, config.clone())),
-			peers: Arc::new(Peers::new(PeerStore::new(db_root)?, adapter, config)),
+			peers: Arc::new(Peers::new(
+				PeerStore::new(db_root)?,
+				adapter,
+				config,
+				stop_state.clone(),
+			)),
 			stop_state,
 		})
 	}
