8c652de464301989a98fc52ae8b1978798bbb2e7
cgilliard <christopher.gilliard@gmail.com>
2020-02-05 18:42:31 -0400

    fix deadlock and blocking issues

--- servers/src/mining/stratumserver.rs
+++ servers/src/mining/stratumserver.rs
@@ -720,7 +720,7 @@ impl WorkersList {
 	}
 
 	pub fn add_worker(&self, tx: Tx) -> usize {
-		//let mut workers_list = self.workers_list.write();
+		let mut workers_list = self.workers_list.write();
 		let mut stratum_stats = self.stratum_stats.write();
 		// Original grin code allways add a new item into the records. It is not good if we have unstable worker.
 		// Or just somebody want to attack the mining pool.
@@ -735,7 +735,6 @@ impl WorkersList {
 
 		let worker = Worker::new(worker_id, tx);
 		let num_workers_val = {
-			let mut workers_list = self.workers_list.write();
 			workers_list.insert(worker_id, worker);
 			workers_list.len()
 		};
@@ -801,12 +800,10 @@ impl WorkersList {
 	}
 
 	pub fn send_to(&self, worker_id: usize, msg: String) {
-		if let Some(worker) = self.get_worker(worker_id) {
-			match worker.tx.unbounded_send(msg) {
-				Err(_) => warn!("Unable to send message to the worker ID {}", worker_id),
-				_ => (),
-			}
-		}
+		let rlock = self.workers_list.read();
+		let tx = rlock.get(&worker_id).unwrap().tx.clone();
+		drop(rlock);
+		tx.unbounded_send(msg).expect("send failed");
 	}
 
 	pub fn broadcast(&self, msg: String) {
