82f361d50a8bf627855b46e769489f82fdc51461
bayk <konstantin.bay@gmail.com>
2020-11-20 20:28:43 -0800

    build issues

--- windows_test.bat
+++ windows_test.bat
@@ -1,3 +1,11 @@
+dir "C:\Program Files (x86)"
+dir "C:\Program Files (x86)\Microsoft Visual Studio"
+dir "C:\Program Files (x86)\Microsoft Visual Studio\2019"
+dir "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community"
+dir "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC"
+dir "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary"
+dir "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build"
+
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
 
 cargo test --all
