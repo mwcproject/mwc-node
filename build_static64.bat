31e6829b4e7d0351eb3ddeb2398d39047df835b1
bayk <konstantin.bay@gmail.com>
2020-11-21 15:51:48 -0800

    Update build, remove redundancy

--- build_static64.bat
+++ build_static64.bat
@@ -5,4 +5,10 @@ set OPENSSL_INCLUDE_DIR="C:\Program Files\OpenSSL-Win64\include"
 set OPENSSL_STATIC="yes"
 set LIBCLANG_PATH="C:\Program Files\LLVM\bin\libclang.dll"
 
+dir "C:\Program Files\"
+dir "C:\Program Files\LLVM\"
+dir "C:\Program Files\LLVM\bin\"
+
+SET
+
 call .ci\win64_cargo.bat build --release
